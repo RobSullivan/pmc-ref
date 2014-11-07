8ca2c2d2c35364481acc8e83ff3539165553690a
Robert Sullivan <r.sullivan@nature.com>
2014-11-07 23:51:56 +0000

    Updated About and API pages. Added newrelic for app monitoring.

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['PMC reference checker'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '0e0f9fd324d3ed9390d22a4970cf29dc584b8ace',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
